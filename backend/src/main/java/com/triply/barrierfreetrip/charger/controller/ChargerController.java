d538471f688c4caa3160f78fa171334e11d04bad
mjjuung <mjjunghp@naver.com>
2024-11-14 23:08:21 +0900

    feat: 로그인 로직 수정 서버에서 처리 -> 클라이언트 처리 후 서버에 저장

--- backend/src/main/java/com/triply/barrierfreetrip/charger/controller/ChargerController.java
+++ backend/src/main/java/com/triply/barrierfreetrip/charger/controller/ChargerController.java
@@ -5,6 +5,7 @@ import com.triply.barrierfreetrip.charger.dto.ChargerInfoDto;
 import com.triply.barrierfreetrip.charger.dto.ChargerListDto;
 import com.triply.barrierfreetrip.charger.service.ChargerService;
 import com.triply.barrierfreetrip.member.domain.Member;
+import com.triply.barrierfreetrip.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +20,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChargerController {
     private final ChargerService chargerService;
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/chargers/{areaCode}")
-    public ResponseEntity returnChargerList(@PathVariable("areaCode") String areaCode) {
+    @GetMapping("/chargers/{sido}/{sigungu}")
+    public ResponseEntity returnChargerList(@PathVariable("sido") String sido,
+                                            @PathVariable("sigungu") String sigungu) {
+        //Member member = memberRepository.findById(Long.valueOf(41)).get();
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        List<ChargerListDto> result = chargerService.returnListDto(member, areaCode);
+        List<ChargerListDto> result = chargerService.returnListDto(member, sido, sigungu);
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
 
@@ -31,6 +35,7 @@ public class ChargerController {
 
     @GetMapping("/chargers/info/{contentId}")
     public ResponseEntity returnChargerInfo(@PathVariable("contentId") Long contentId) {
+        //Member member = memberRepository.findById(Long.valueOf(41)).get();
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         ChargerInfoDto result = chargerService.returnChargerInfo(member, contentId);
 
